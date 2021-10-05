ad2db4607c566013f641ea7d2b870001dfbb8007
trantho <tranvanthotinb@yahoo.com>
2021-10-05 14:47:13 +0700

    add torch code

--- tensor_initialization/Tensor_Indexing.py
+++ tensor_initialization/Tensor_Indexing.py
@@ -1,1 +1,44 @@
 import torch
+
+#===================================================================================#
+#                       Tensor indexing                                             #
+#===================================================================================#
+
+batch_size = 10
+features = 25
+
+x = torch.rand((batch_size,features))
+
+print(x[0].shape) #x[0,:]
+print(x[:,0].shape)
+print(x[2,0:10]) #0:10=>[0,1,2,3,4,5,6,7,8,9]
+
+x[0,0] = 100
+
+#Fancy indexing
+x = torch.arange(10)
+indices = [2,5,8]
+print(x[indices])
+
+x = torch.rand((3,5))
+rows = torch.tensor([1,0])
+cols = torch.tensor([4,0])
+
+print(x[rows,cols])
+
+#more advantage indexing
+x = torch.arange(10)
+print(x[(x<2)|(x>8)])
+print(x[x.remainder(2)==0])
+
+#useful operations
+print(torch.where(x>5,x,x*2))
+print(torch.tensor([0,0,1,2,2,3,4]).unique())
+print(x.ndimension()) #5x5x5
+print(x.numel())
+
+
+
+
+
+
